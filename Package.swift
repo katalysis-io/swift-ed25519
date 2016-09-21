08ece6a2be990d475a9e23952921a5ddb950e16b
Alex <alex@katalysis.io>
2016-09-21 16:03:59 +0000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,21 +1,9 @@
 import PackageDescription
-#if os(OSX)
-let url = "https://github.com/IBM-Swift/CommonCrypto.git"
-let majorVersion = 0
-let minorVersion = 1
-#elseif os(Linux)
-let url = "https://github.com/IBM-Swift/OpenSSL.git"
-let majorVersion = 0
-let minorVersion = 2
-#else
-fatalError("Unsupported OS")
-#endif
 
 let package = Package(
   name: "Ed25519",
   dependencies: [
-    .Package(url: url, majorVersion: majorVersion, minor: minorVersion),
-    .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 5),
+    .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 7),
     ],
   exclude: ["Cryptor.xcodeproj", "README.md", "Sources/Info.plist"]
 )
