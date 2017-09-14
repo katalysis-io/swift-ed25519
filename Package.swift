0cb67dc3ea4ff59564fe160041700424d6f8b7a0
Alex Tran Qui <alex@katalysis.io>
2017-09-14 18:37:04 +0200

    Upgrade to Swift 4.0 compiler and CryptoSwift 0.7.x

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Ed25519" ,
   dependencies: [
-    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 6),
+    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 7),
     ]
 )
 
